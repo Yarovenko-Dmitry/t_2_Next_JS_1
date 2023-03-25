58dd0c1f4cde702bc30229acb088b1d7b5e8fa8a
dmitry <yarovenkojob@gmail.com>
2023-03-25 22:51:52 +0300

    feat: add project

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function Error() {
+    return (
+        <h1>
+           что-то пошло не так
+        </h1>
+    )
+};
---
+++ pages/users.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import MainContainer from "../components/MainContainer";
+
+const Users = ({users}) => {
+
+    return (
+        <MainContainer keywords={"users next js"}>
+            <h1>Cписок пользователей</h1>
+            <ul>
+                {users.map(user =>
+                    <li key={user.id}>
+                        <Link legacyBehavior href={`/users/${user.id}`}>
+                            <a>{user.name}</a>
+                            {/*<Link legacyBehavior >{user.name}</Link >*/}
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </MainContainer>
+    );
+};
+
+export default Users;
+
+export async function getStaticProps(context) {
+    console.log(' context :', context)
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const users = await response.json()
+    // console.log(' users :', users)
+
+    return {
+        props: {users}, // will be passed to the page component as props
+    }
+}
